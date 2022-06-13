a39c5376c81ba6f2c668a207aa97de177ef9920a
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-06-13 14:07:22 +0500

    Added validation exception handlers

--- src/main/java/kh/farrukh/progee_api/exception/ApiExceptionHandler.java
+++ src/main/java/kh/farrukh/progee_api/exception/ApiExceptionHandler.java
@@ -5,10 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolationException;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Locale;
 
 @ControllerAdvice
@@ -32,6 +36,40 @@ public class ApiExceptionHandler {
                         ZonedDateTime.now()
                 ),
                 exception.getHttpStatus()
+        );
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleSortParamValidationException(ConstraintViolationException exception) {
+        String errorMessage = new ArrayList<>(exception.getConstraintViolations()).get(0).getMessage();
+        return new ResponseEntity<>(
+                new ErrorResponse(
+                        errorMessage,
+                        HttpStatus.BAD_REQUEST,
+                        HttpStatus.BAD_REQUEST.value(),
+                        ZonedDateTime.now()
+                ),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleRequestBodyValidationException(MethodArgumentNotValidException exception) {
+        FieldError error = exception.getFieldError();
+        String errorMessage;
+        if (error != null) {
+            errorMessage = error.getField() + " " + error.getDefaultMessage();
+        } else {
+            errorMessage = exception.getAllErrors().get(0).toString();
+        }
+        return new ResponseEntity<>(
+                new ErrorResponse(
+                        errorMessage,
+                        HttpStatus.BAD_REQUEST,
+                        HttpStatus.BAD_REQUEST.value(),
+                        ZonedDateTime.now()
+                ),
+                HttpStatus.BAD_REQUEST
         );
     }
 
