96b5634dd181aa8dc8d36888f7af39959501435a
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-07-09 02:52:07 +0500

    Added ReviewController integration tests

---
+++ src/test/java/kh/farrukh/progee_api/test_utils/ReviewValueSerializer.java
@@ -0,0 +1,20 @@
+package kh.farrukh.progee_api.test_utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import kh.farrukh.progee_api.endpoints.review.ReviewValue;
+
+import java.io.IOException;
+
+public class ReviewValueSerializer extends StdSerializer<ReviewValue> {
+
+    public ReviewValueSerializer() {
+        super(ReviewValue.class);
+    }
+
+    @Override
+    public void serialize(ReviewValue value, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException {
+        jsonGenerator.writeNumber(value.getScore());
+    }
+}
