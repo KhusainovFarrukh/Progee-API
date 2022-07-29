edf8e9ef9f1768898a62c3a3565bcbcb6838f0c7
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-07-29 16:27:36 +0500

    Added temp solution in AuthenticationEntryPoint

---
+++ src/main/java/kh/farrukh/progee_api/exception/custom_exceptions/InvalidCredentialsException.java
@@ -0,0 +1,29 @@
+package kh.farrukh.progee_api.exception.custom_exceptions;
+
+import kh.farrukh.progee_api.exception.ApiException;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static kh.farrukh.progee_api.utils.constant.ExceptionMessages.EXCEPTION_INVALID_CREDENTIALS;
+
+/**
+ * `InvalidCredentialsException` is a subclass of `ApiException` that is thrown
+ * when token or email/password is invalid
+ * <p>
+ * HttpStatus of the response will be UNAUTHORIZED
+ * <p>
+ * todo project needs separate exceptions for invalid token and invalid email/password (EmailPasswordInvalidException: ready)
+ *      see todos in EmailPasswordAuthenticationEntryPoint
+ */
+@Getter
+public class InvalidCredentialsException extends ApiException {
+
+    public InvalidCredentialsException() {
+        super(
+                "Token is invalid. Get the new one from login endpoint",
+                HttpStatus.UNAUTHORIZED,
+                EXCEPTION_INVALID_CREDENTIALS,
+                new Object[]{}
+        );
+    }
+}
--- src/main/java/kh/farrukh/progee_api/security/handlers/EmailPasswordAuthenticationEntryPoint.java
+++ src/main/java/kh/farrukh/progee_api/security/handlers/EmailPasswordAuthenticationEntryPoint.java
@@ -1,7 +1,6 @@
 package kh.farrukh.progee_api.security.handlers;
 
-import kh.farrukh.progee_api.endpoints.user.UserRepository;
-import kh.farrukh.progee_api.exception.custom_exceptions.EmailPasswordInvalidException;
+import kh.farrukh.progee_api.exception.custom_exceptions.InvalidCredentialsException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -14,15 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class EmailPasswordAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    // TODO: 7/29/22 change to service
-    private final UserRepository userRepository;
+    //    // TODO: 7/29/22 change to service
+//    private final UserRepository userRepository;
     private final HandlerExceptionResolver resolver;
 
     public EmailPasswordAuthenticationEntryPoint(
-            UserRepository userRepository,
+//            UserRepository userRepository,
             @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver
     ) {
-        this.userRepository = userRepository;
+//        this.userRepository = userRepository;
         this.resolver = resolver;
     }
 
@@ -32,13 +31,16 @@ public class EmailPasswordAuthenticationEntryPoint implements AuthenticationEntr
             HttpServletResponse response,
             AuthenticationException authException
     ) {
-        EmailPasswordInvalidException.Type type;
-        // TODO: 7/29/22 always being false because email is null
-        if (userRepository.existsByEmail(request.getParameter("email"))) {
-            type = EmailPasswordInvalidException.Type.PASSWORD;
-        } else {
-            type = EmailPasswordInvalidException.Type.EMAIL;
-        }
-        resolver.resolveException(request, response, null, new EmailPasswordInvalidException(type));
+//        // TODO: 7/29/22 separate message (logic) for 401 in /login and 401 in other endpoints
+//        EmailPasswordInvalidException.Type type;
+//        // TODO: 7/29/22 always being false because email is null
+//        if (userRepository.existsByEmail(request.getParameter("email"))) {
+//            type = EmailPasswordInvalidException.Type.PASSWORD;
+//        } else {
+//            type = EmailPasswordInvalidException.Type.EMAIL;
+//        }
+//        resolver.resolveException(request, response, null, new EmailPasswordInvalidException(type));
+
+        resolver.resolveException(request, response, null, new InvalidCredentialsException());
     }
 }
