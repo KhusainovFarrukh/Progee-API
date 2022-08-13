7a4a84979f0382a875255e94426f353e1cb1bee3
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-08-13 21:27:56 +0500

    Fixed db migration bug

--- src/main/resources/db/migration/V6_7__rename_delete_review_permission.sql
+++ src/main/resources/db/migration/V6_7__rename_delete_review_permission.sql
@@ -1,3 +1,9 @@
 UPDATE role_permissions
-SET permission_name = 'CAN_DELETE_REVIEW'
-WHERE permission_name = 'CAN_DELETE_OWN_REVIEW';
+SET permission_name = 'CAN_DELETE_OWN_REVIEW'
+WHERE permission_name = 'CAN_DELETE_REVIEW';
+
+INSERT INTO role_permissions (role_id, permission_name)
+VALUES (1, 'CAN_DELETE_OTHERS_REVIEW');
+
+INSERT INTO role_permissions (role_id, permission_name)
+VALUES (2, 'CAN_DELETE_OTHERS_REVIEW');
