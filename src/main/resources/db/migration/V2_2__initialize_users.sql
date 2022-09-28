ca485e4e327465b16c11e45ba55f0941990ad79e
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-09-28 16:51:27 +0500

    Encrypted password in DB migration replaced with flyway placeholder (environment variable)

--- src/main/resources/db/migration/V2_2__initialize_users.sql
+++ src/main/resources/db/migration/V2_2__initialize_users.sql
@@ -2,8 +2,8 @@ ALTER SEQUENCE users_id_seq RESTART WITH 1;
 
 INSERT INTO users (name, email, username, password, is_enabled, is_locked, role)
 VALUES ('Farrukh Khusainov', 'farrukhbekkhusainov@gmail.com', 'farrukh_kh',
-        '$2a$10$u.olISwSqjbaZCHADL0fIuw7eBijpqzvfSavgXnPcfniJTwORGNvm', TRUE, FALSE, 'SUPER_ADMIN'),
+        '${DEFAULT_ENCRYPTED_PASSWORD}', TRUE, FALSE, 'SUPER_ADMIN'),
        ('Hamdam Xudayberganov', 'hamdam@mail.com', 'hamdam_x',
-        '$2a$10$u.olISwSqjbaZCHADL0fIuw7eBijpqzvfSavgXnPcfniJTwORGNvm', TRUE, FALSE, 'ADMIN'),
+        '${DEFAULT_ENCRYPTED_PASSWORD}', TRUE, FALSE, 'ADMIN'),
        ('User Tester', 'user@mail.com', 'user_tester',
-        '$2a$10$u.olISwSqjbaZCHADL0fIuw7eBijpqzvfSavgXnPcfniJTwORGNvm', TRUE, FALSE, 'USER');
+        '${DEFAULT_ENCRYPTED_PASSWORD}', TRUE, FALSE, 'USER');
