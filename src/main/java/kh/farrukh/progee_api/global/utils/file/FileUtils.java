5f7ae904a047f86c35a28f0bf2583f46d261bd43
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-10-18 21:58:31 +0500

    Added comments to code

--- src/main/java/kh/farrukh/progee_api/global/utils/file/FileUtils.java
+++ src/main/java/kh/farrukh/progee_api/global/utils/file/FileUtils.java
@@ -5,6 +5,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileUtils {
 
+    /**
+     * It takes a file name, removes the extension, replaces spaces with underscores, and appends the current time in
+     * milliseconds to the end of the file name
+     *
+     * @param image The image file that you want to upload.
+     * @return A unique image name.
+     */
     public static String getUniqueImageName(MultipartFile image) {
         String originalFilename = image.getOriginalFilename();
         String nameWithoutExtension = FilenameUtils.removeExtension(originalFilename);
